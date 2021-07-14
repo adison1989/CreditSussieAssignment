1d9b558f4e905b98c9844cd270fe3ba7175149fa
Aditya Soni <aditya12111989@gmail.com>
2021-07-14 21:48:30 +0530

    Add files via upload

---
+++ src/main/java/com/assignment/creditSussie/CreditSussieApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.creditSussie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSussieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditSussieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/creditSussie/CreditSussieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.creditSussie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSussieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
